cfc3f55dc758c1df454735282f7f4d4f603d2a73
CLARbel <cnhepure16@gmail.com>
2023-03-16 15:35:04 -0700

    FIX C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"
- * Return: 0
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
