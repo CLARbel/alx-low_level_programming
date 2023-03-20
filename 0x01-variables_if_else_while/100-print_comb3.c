96c1088f47b7c551b34abb6a5b18490c7f0a888d
CLARbel <cnhepure16@gmail.com>
2023-03-20 16:35:31 -0700

    100 print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,9 +2,30 @@
 
 /**
  * main - Entry point
- * Description:
+ * Description: 'Prints all different combinations of two digit numbers'
  * Return: Always 0
  */
 int main(void)
 {
+	int i;
+	int j;
 
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
